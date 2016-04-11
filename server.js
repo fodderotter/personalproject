d256578592748ada32449c6c28b19be35ff157d8
Daniel Seol <seol.daniel@gmail.com>
2016-04-10 20:20:17 -0500

    adding heroku hosting

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ var   express = require('express')
 	, cors = require('cors')
 	, mongoose = require('mongoose')
 	, port = process.env.PORT || 9000
+	, mongoUri = "mongodb://heroku_mn11f7hq:abcd1234@ds021010.mlab.com:21010/heroku_mn11f7hq"
 	// , mongoUri = 'mongodb://localhost:27017/projects'
     , morgan = require('morgan');
 
-    
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'))
-// mongoose.connect(mongoUri);
+mongoose.connect(mongoUri);
 mongoose.connection.once('open', function() {
 	console.log('Connected to MongoDB at ' + mongoUri);
 });

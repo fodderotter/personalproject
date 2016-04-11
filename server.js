9aff6e82515529ac95913730fd0ade683ff339a9
Daniel Seol <seol.daniel@gmail.com>
2016-04-10 20:11:10 -0500

    adding heroku hosting

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var   express = require('express')
 	, cors = require('cors')
 	, mongoose = require('mongoose')
 	, port = process.env.PORT || 9000
-	// , mongoUri = 'mongodb://localhost:27017/projects'
+	, mongoUri = 'mongodb://localhost:27017/projects'
     , morgan = require('morgan');
 app.use(bodyParser.json());
 app.use(cors());
